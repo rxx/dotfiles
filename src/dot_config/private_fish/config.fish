3bbfad7a42db1b0d2b05d7a96f9292bc0d5df597
Maksym Litvynov <1058433+rxx@users.noreply.github.com>
2024-03-08 22:17:56 +0200

    Configure fish

--- src/dot_config/private_fish/config.fish
+++ src/dot_config/private_fish/config.fish
@@ -1,3 +1,37 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Editor Neo Vim
+set -gx EDITOR nvim
+abbr --add vim nvim
+abbr --add vi nvim
+
+# Chezmai Dotfiles
+abbr --add d chezmoi 
+abbr --add dedit chezmoi --edit --apply 
+
+# Git
+abbr --add g git
+abbr --add ga git add
+abbr --add gd git diff
+abbr --add gdm git diff main
+abbr --add gds git diff --staged 
+abbr --add gci git commit -m 
+abbr --add gaci git commit -am 
+abbr --add gg git status 
+abbr --add gf git fetch -p 
+abbr --add gco git checkout 
+abbr --add gcob git checkout -b 
+abbr --add gcm git checkout main 
+abbr --add gpu git push -u 
+abbr --add gpf git push -f 
+abbr --add glp git log -p
+abbr --add grbc git rebase --continue 
+abbr --add grbs git rebase --skip 
+abbr --add grba git rebase --abort 
+abbr --add gbr git branch 
+abbr --add gl git pull 
+abbr --add gup git pull --rebase 
+
+abbr --add dni sudo dnf install -y 
