e5fd0c55ce02aef814759117757c49150f4df254
Maksym Litvynov <1058433+rxx@users.noreply.github.com>
2024-03-19 16:39:36 +0200

    Install AstroVim as main NeoVim

--- src/dot_config/nvim/lua/plugins/treesitter.lua
+++ src/dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,16 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = { "RRethy/nvim-treesitter-endwise" },
-    opts = function(_, opts)
-      opts.endwise = { enable = true }
-      opts.indent = { enable = true, disable = { "yaml", "ruby" } }
-      opts.ensure_installed = {
-        "bash",
-        "embedded_template",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "ruby",
-        "ruby-lsp",
-        "tsx",
-        "typescript",
-        "yaml",
-      }
-    end,
-  },
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
+    })
+  end,
 }
