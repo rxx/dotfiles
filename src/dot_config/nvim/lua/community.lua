d8082b71e83348f158c396805d720b6378f14820
Maksym Litvynov <1058433+rxx@users.noreply.github.com>
2024-03-19 20:58:37 +0200

    Configured AstroVim

--- src/dot_config/nvim/lua/community.lua
+++ src/dot_config/nvim/lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -9,5 +7,16 @@ return {
   -- TODO: Remove branch v4 on release
   { "AstroNvim/astrocommunity", branch = "v4" },
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.bash" },
+  --  { import = "astrocommunity.pack.docker" },
+  -- { import = "astrocommunity.pack.go" },
+  -- { import = "astrocommunity.pack.ruby" },
+  --  { import = "astrocommunity.pack.tailwindcss" },
+  --  { import = "astrocommunity.pack.typescript" },
+  -- { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.editing-support.nvim-treesitter-endwise" },
 }
--- src/dot_config/nvim/lua/plugins/vim-rails.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { "tpope/vim-rails" },
-  { "tpope/vim-dadbod" },
-  { "tpope/vim-bundler" },
-  { "tpope/vim-dispatch" },
-  { "tpope/vim-rake" },
-}
