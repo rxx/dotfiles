4c3e1677ce8d50fdadf18693d3369ccc69efce07
Maksym Litvynov <1058433+rxx@users.noreply.github.com>
2024-03-19 22:45:14 +0200

    Remove tide in favour to starship

--- src/dot_config/private_fish/config.fish
+++ src/dot_config/private_fish/config.fish
@@ -5,6 +5,3 @@ end
 set -U fish_greeting
 
 abbr --add dni sudo dnf install -y 
-
-# starship init fish | source
-# string match -q "$TERM_PROGRAM" "vscode" and . (code --locate-shell-integration-path fish)
