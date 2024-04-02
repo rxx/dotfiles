3605b8c937a3e29bcd7949149c9e6dcf9a88a094
Maksym Litvynov <1058433+rxx@users.noreply.github.com>
2024-04-02 21:31:27 +0300

    Add tailwind sorter to neovim

---
+++ src/dot_config/nvim/lua/plugins/tailwind-sorter.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "laytan/tailwind-sorter.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
+    build = "cd formatter && npm ci && npm run build",
+    config = {
+      on_save_enabled = true,
+    },
+  },
+}
